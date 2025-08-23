2163a0a67f7ae31faed8357b170abdfbebab2228
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-08-23 01:22:01 -0500

    Second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,17 @@ import './App.css'
 
 function App() {
   return (
+    <>
+    <Text display_content="Hii.."/>
+    <Text display_content="Hello World"/>
+    </>
+  )
+}
+
+function Text({display_content}) {
+  return (
     <div>
-      <p>Hello World</p>
+      <p>{display_content}</p>
     </div>
   )
 }
