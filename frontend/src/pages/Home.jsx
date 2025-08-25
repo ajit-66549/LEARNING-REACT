9a213226310efe0f36542c62dbf099b4bfd978d4
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-08-25 13:53:48 -0500

    Defined state (basics)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,43 @@
+import { use } from "react";
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
-    const movies = [
-        {id: 1, title: "The Notebook", release_date: 2004},
-        {id: 2, title: "La La Land", release_date: 2016},
-        {id: 3, title: "A Walk to Remember", release_date: 2002},
-    ];
+  const [searchQuery, setsearchQuery] = useState("");
 
-    const handleSearch = () => {
+  const movies = [
+    { id: 1, title: "The Notebook", release_date: 2004 },
+    { id: 2, title: "La La Land", release_date: 2016 },
+    { id: 3, title: "A Walk to Remember", release_date: 2002 },
+  ];
 
-    }
+  const handleSearch = (e) => {
+    e.preventDefault()    //prevents default behavior like refreshing the page
+    alert(searchQuery)
+  };
 
-    return (<div className="home">
-        <form className="search-form" onSubmit={handleSearch}>
-            <input type="text" placeholder="Search for movies..." className="movie-input" />
-            <button type="submit" className="search-button">Search</button>
-        </form>
+  return (
+    <div className="home">
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="movie-input"
+          value={searchQuery}
+          onChange={(e) => setsearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
 
-        <div className="movies-grid">
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id}/>
-            ))}
-        </div>
-    </div>)
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
