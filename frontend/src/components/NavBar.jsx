04f5decc8284e93400c4dd958f9b22b1429f7124
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-09-03 20:00:00 -0500

    Did some styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import "../css/Navbar.css"
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand"><h1>Movie App</h1></div>
+      <div className="navbar-brand">Movie App</div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
           Home
-        </Link> <br/>
+        </Link>
         <Link to="/favorites" className="nav-link">
           Favorites
         </Link>
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,8 @@
+import "../css/Favorites.css"
+
 function Favorites() {
 
-    return <div className="favorite-empty">
+    return <div className="favorites-empty">
         <h2>No Favorite movies yet</h2>
         <p>Start adding favorite movies and they will appear here</p>
     </div>
