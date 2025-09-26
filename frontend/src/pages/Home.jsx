c9de882dc2a9461566f6725f0a7abf41b4e9d7e1
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-09-26 18:53:41 -0500

    Set error and loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import MovieCard from "../components/MovieCard";
 import { useState, useEffect } from "react";
 import { getPopularMovies, searchMovies } from "../services/api";
-import "../css/Home.css"
+import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setsearchQuery] = useState("");
@@ -12,18 +12,18 @@ function Home() {
   useEffect(() => {
     const loadPopularMovies = async () => {
       try {
-        const popularMovies = await getPopularMovies()
-        setMovies(popularMovies)
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
       } catch (err) {
         console.log(err);
-        setError("Failed to load movies.....")
+        setError("Failed to load movies.....");
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
-    
-    loadPopularMovies()
-  }, [])
+    };
+
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault(); //prevents default behavior like refreshing the page
@@ -45,14 +45,20 @@ function Home() {
         </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id} />
-            )
-        )}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading......</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
